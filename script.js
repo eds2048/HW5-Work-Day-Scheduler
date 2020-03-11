86ced0d3adbea95a8328f43a43d8ad23bdfe6cc2
Ed Son <esben90210@gmail.com>
2020-03-11 18:17:28 -0400

    update

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
 
+// Create variables from moment API
+var fulldate = moment().format("MMM Do YY"); 
+var currentDay = moment().format('DDD');
+var currentHour = moment().format('hh');
 
-
-var today = moment().format('DDD');
-var hour = moment().format('hh');
-var todaysAgenda = localStorage.getItem(today);
-localStorage.setItem(today, '{"hr9": "get up","hr10":"past my alarm"}');
-
+// Store day into local storage
+var todaysAgenda = localStorage.getItem(currentDay);
+//localStorage.setItem(currentDay, '{"hr9": "get up","hr10":"past my alarm"}');
 console.log("today's agenda " + todaysAgenda);
-console.log("hour " + hour);
-console.log("today " + today);
+console.log("hour " + currentHour);
+console.log("today " + currentDay);
 console.log(JSON.parse(todaysAgenda));
 
-
-
-
+// Display current day at top
+$("#todays-date").append(fulldate);
 // check the activities w the adding lists using jQuery
-// Use moment.js & jQuery
-// use bootstrap
-    // create column for each day?
-    // create rows for each hour?
-    // columns for the hour, task then add button
+// Past timeblock should be different color
+
+var testForHour = document.getElementById("hour-9").getAttribute("value");
+console.log("TEST FOR VALUE " + testForHour);
+
+
+//On click event of saveBtns
+$(".btn").on("click", function(){
+// Save entered text in description into local storage
+// var userInput = $("#textarea").val();
+// console.log("User Input " + userInput);
+
+var textInput = JSON.parse(localStorage.getItem("textarea"));
+console.log("text input "+  textInput);
+
+//var userInput = localStorage.getItem("textarea".value);
+
+// console.log(userInput);
+// console.log("button worked")
+});
+
+
+// save input text from text area into local storage
+// - event listener? for time block
+    
 
 
 
-
-// launch application
-// display current day
-// scroll down, display hours
-// timeblock for each hour
-    // past timeblock - different color
-
-//click on event - event listener? for time block
 // take text input from user
 // click on enter button to save - need event listener for enter button
 
